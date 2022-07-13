c98095822d2d6cb1b9b485d0e22cc5d9f51a9b08
Jaky-Dev <joelangkaiyu@gmail.com>
2022-07-13 13:25:59 +0800

    Changed database

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection ({
   host: "localhost",
   user: "root",
   password: "Password",
-  database: "mySmartHome" });
+  database: "" });
  // connect to database
  db.connect((err) => {
   if (err) {
